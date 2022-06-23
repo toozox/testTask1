46dde48469f36c35af2449d44e50943482795933
Sergey Nikiforov <nik_ser91@mail.ru>
2022-06-23 16:40:04 +0500

    Add simple GUI interface

---
+++ genPyFromUi.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+pyuic5 -x mainWindow.ui -o mainWindowUi.py
+
+echo "mainWindow.ui -> mainWindowUi.py"
--- main.py
+++ main.py
@@ -1,6 +1,18 @@
+from PyQt5 import QtWidgets
+from mainWindowUi import Ui_MainWindow
+
+
+class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.setupUi(self)
+
 
 def main():
-    pass
+    app = QtWidgets.QApplication([])
+    w = MainWindow()
+    w.show()
+    exit(app.exec())
 
 
 if __name__ == "__main__":
